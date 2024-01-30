e7364c183a077a6af7d517893a1c0e635dc30734
KhoaPhamzzz <102586997@student.swin.edu.au>
2024-01-30 19:38:41 +1100

    email final

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,28 +2,49 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
-const fromEmail = process.env.NEXT_PUBLIC_FROM_EMAIL;
+const fromEmail = process.env.NEXT_PUBLIC_FROM_EMAIL; // Your verified email
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
+
   try {
-    const data = await resend.emails.send({
+    // Sending the actual message to your email
+    await resend.emails.send({
       from: fromEmail,
-      to: [fromEmail, email],
-      subject: subject,
+      replyTo: email, // Setting the Reply-To header to the sender's email
+      to: ["akpham28@gmail.com", "AK@khoaphams.com"],
+      subject: `New message from ${email}: ${subject}`,
+      headers: {
+        "Reply-To": email, // Set the Reply-To header
+      },
       react: (
         <>
           <h1>{subject}</h1>
-          <p>Thank you for contacting Me! I have received your message and will get back to you soon.</p>
-          <br/>
-          <br/>
-          <p>New message submitted:</p>
+          <p>Sender's email: {email}</p>
           <p>{message}</p>
         </>
       ),
     });
-    return NextResponse.json(data);
+
+    // Sending a thank you message to the sender
+    await resend.emails.send({
+      from: fromEmail,
+      to: [email],
+      subject: `Thank you for contacting me!`,
+      react: (
+        <>
+          <h1>Thank you for reaching out!</h1>
+          <p>Hi, I have received your message regarding: {subject}.</p>
+          <p>I'll get back to you as soon as possible.</p>
+          <br/>
+          <p>Best regards,</p>
+          <p>[Khoa Anh Pham]</p>
+        </>
+      ),
+    });
+
+    return NextResponse.json({ message: "Emails sent successfully" });
   } catch (error) {
     return NextResponse.json({ error });
   }
